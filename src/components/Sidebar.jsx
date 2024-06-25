ae461145fe5252d1034454887f3ba7b9cd45b0f1
kurlerprudent <osarkodie@st.ug.edu.gh>
2024-06-25 14:59:50 +0000

    Added a side navBar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,89 @@
-import { Box } from '@mui/material'
+import { Avatar, Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material'
 import React from 'react'
+import {CalendarToday, Cast, DarkMode, Handshake, Home, Settings, VolunteerActivism} from "@mui/icons-material";
+
+
 
 const Sidebar = () => {
   return (
-    <Box flex={1} bgcolor="blue" p={2} sx={{display:{xs:"none" , sm:"block"}}}>
-        Side Bar
+    <Box flex={1} p={2} sx={{display:{xs:"none" , sm:"block"}}}>
+         <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItemButton>
+            </ListItem>
+            
+
+            
+            <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <CalendarToday />
+              </ListItemIcon>
+              <ListItemText primary="Events" />
+            </ListItemButton>
+            </ListItem>
+            
+
+            
+            <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Handshake />
+              </ListItemIcon>
+              <ListItemText primary="Partnership" />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <VolunteerActivism />
+              </ListItemIcon>
+              <ListItemText primary="Donation" />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Cast />
+              </ListItemIcon>
+              <ListItemText primary="Live Stream" />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Avatar sx={{width:30, height:30}}/>
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <DarkMode />
+              </ListItemIcon>
+              <Switch/>
+            </ListItemButton>
+          </ListItem>
+          </List>
     </Box>
   )
 }
